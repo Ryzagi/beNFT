f19e68cf169c0fefb2a701fc3bb47ed935c6a12a
paveltbb <anisimov.py@truercm.com>
2023-05-08 15:59:38 +0300

    git init

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+from pathlib import Path
+import sqlite3
+from fastapi import FastAPI
+from langchain.document_loaders import PyPDFLoader
+from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from data import Message
+
+MESSAGE_ENDPOINT = "/api/message"
+model_name_embeddings = "text-embedding-ada-002"
+
+# Create datastore
+if os.path.exists("data_store"):
+    vector_store = FAISS.load_local(
+        "data_store",
+        OpenAIEmbeddings()
+    )
+else:
+    file = "BeNFT.pdf"
+    loader = PyPDFLoader(file)
+    input_text = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(input_text, embeddings)
+    # Save the files `to local disk.
+    vector_store.save_local("data_store")
+
+print("Index is built")
+
+system_template = """Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that "I don't know", don't try to make up an answer.
+----------------
+{summaries}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}")
+]
+prompt = ChatPromptTemplate.from_messages(messages)
+
+chain_type_kwargs = {"prompt": prompt}
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,
+                 max_tokens=256)  # Modify model_name if you have access to GPT-4
+chain = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
+
+app = FastAPI()
+
+
+# Define the endpoint for handling queries
+@app.post(MESSAGE_ENDPOINT)
+async def handle_query(request: Message):
+    result_text = chain(request.message)
+    return {"result": result_text['answer']}
---
+++ discord_run.py
@@ -0,0 +1,94 @@
+import argparse
+import asyncio
+import logging
+import aiohttp
+import base64
+import discord
+
+from io import BytesIO
+from pathlib import Path
+
+from discord.ext.commands import Bot
+from discord import Message
+
+intents = discord.Intents.default()
+intents.members = True
+bot = Bot(command_prefix='/', intents=intents)
+MAX_MESSAGE_LENGTH = 2000
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--discord_token", help="Discord_token bot token", type=str, required=True
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+
+
+def try_(func):
+    async def try_except(message):
+        error = ""
+        for i in range(4):
+            try:
+                await func(message)
+                return None
+            except Exception as e:
+                print(e)
+                error = str(e).lower()
+                pass
+            await asyncio.sleep(1)
+        if "overloaded with other requests" in error:
+
+            await message.channel.send("\nPlease, try again later, We are currently under heavy load")
+        else:
+
+            await message.channel.send('\nSomething went wrong, please type "/start" to start over')
+        return None
+
+    return try_except
+
+
+@bot.command(name='start')
+async def start(ctx):
+    """
+    This command will be called when user sends !start command
+    """
+    author_id = ctx.author.id
+    await ctx.channel.typing()
+    await asyncio.sleep(1)
+    await ctx.channel.send("Welcome to BeNFT!\nAsk your question and I'll answer it!")
+
+
+@bot.event
+async def on_message(message: Message) -> None:
+    if message.author == bot.user:
+        return
+    if not isinstance(message.channel, discord.DMChannel):
+        return
+
+    if message.content.startswith('/start'):
+        await start(message)
+        return
+    async with aiohttp.ClientSession() as session:
+        # Example for MESSAGE_ENDPOINT
+        async with session.post(
+                "http://localhost:8000/api/message",
+                json={"message": message.content},
+        ) as response:
+            chatbot_response = await response.json()
+    await message.channel.typing()
+    num_messages = len(chatbot_response["result"]) // MAX_MESSAGE_LENGTH
+    await message.channel.typing()
+    await asyncio.sleep(1)
+    for i in range(num_messages + 1):
+        await message.channel.typing()
+        await asyncio.sleep(1)
+        await message.channel.send(chatbot_response["result"][i * MAX_MESSAGE_LENGTH: (i + 1) * MAX_MESSAGE_LENGTH])
+
+
+if __name__ == "__main__":
+    bot_token = args.discord_token
+    bot.run(bot_token)
