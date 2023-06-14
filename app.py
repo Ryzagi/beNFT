c641f5d4a4487a3681ec9930dc6752318178be40
Ryzagi <vladislavrzhv@gmail.com>
2023-06-14 11:53:40 +0300

    fix

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 from pathlib import Path
+import glob
 
 import uvicorn
-from fastapi import FastAPI
-from langchain.document_loaders import PyPDFLoader
+from fastapi import FastAPI, UploadFile
+from langchain.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
@@ -13,25 +14,40 @@ from langchain.vectorstores import FAISS
 from data import Message
 from sql import get_info_from_sql, delete_all_from_sql
 from utils import text_to_pdf
+import time
+import json
 
 REFRESH_ENDPOINT = "/api/refresh_data"
 MESSAGE_ENDPOINT = "/api/message"
+PDF_UPLOAD_ENDPOINT = "/api/pdf/upload"
+PDF_LIST_ENDPOINT = "/api/pdf/list"
+PDF_DOWNLOAD_ENDPOING = "/api/pdf/download"
+PDF_DELETE_ENDPOINT = "/api/pdf/delete"
+
 model_name_embeddings = "text-embedding-ada-002"
 THIS_DIR = Path(__file__).parent
+pdf_storage_dir = "pdf_store"
+# Create storage for PDFs
+if os.path.exists(pdf_storage_dir):
+    print("Pdf storage exists")
+else:
+    os.mkdir(pdf_storage_dir)
+    print("Pdf doesn't storage exist\ncreating")
+
 # Create datastore
-#if os.path.exists("data_store"):
-#    vector_store = FAISS.load_local(
-#        "data_store",
-#        OpenAIEmbeddings()
-#    )
-#else:
-#    file = str(THIS_DIR / "beNFT.pdf")
-#    loader = PyPDFLoader(file)
-#    input_text = loader.load_and_split()
-#    embeddings = OpenAIEmbeddings()
-#    vector_store = FAISS.from_documents(input_text, embeddings)
-#    # Save the files `to local disk.
-#    vector_store.save_local("data_store")
+if os.path.exists("data_store"):
+    vector_store = FAISS.load_local(
+        "data_store",
+        OpenAIEmbeddings()
+    )
+else:
+    file = str(THIS_DIR / "beNFT.pdf")
+    loader = PyPDFLoader(file)
+    input_text = loader.load_and_split()
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(input_text, embeddings)
+    # Save the files `to local disk.
+    vector_store.save_local("data_store")
 
 print("Index is built")
 
@@ -85,16 +101,43 @@ async def refresh_data():
 
         text_to_pdf(texts, THIS_DIR / "beNFT.pdf")
 
-        file = str(THIS_DIR / "beNFT.pdf")
+        '''file = str(THIS_DIR / "beNFT.pdf")
         loader = PyPDFLoader(file)
         input_text = loader.load_and_split()
         embeddings = OpenAIEmbeddings()
-        vector_store = FAISS.from_documents(input_text, embeddings)
+        vector_store = FAISS.from_documents(input_text, embeddings)'''
+        pdf_files = glob.glob(os.path.join(pdf_storage_dir, '*.pdf'))
+        pdf_files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+        newest_files = pdf_files[:10]
+        files_used = ""
+        #input_text = None
+        #loader = PyPDFLoader(newest_files)
+        if os.path.exists(pdf_storage_dir + "/tmp"):
+            #print("Pdf storage exists")
+            #os.rmdir
+            files = glob.glob(os.path.join(pdf_storage_dir+"/tmp", '*'))
+            for f in files:
+                os.remove(f)
+        else:
+            os.mkdir(pdf_storage_dir + "/tmp")
 
-        # Save the files `to local disk.
+        for file in newest_files:
+            source_path = os.path.abspath(file)  # Get the absolute path of the file
+            destination_path = os.path.join(pdf_storage_dir + "/tmp", os.path.basename(file))  # Create the destination path in the tmp directory
+            files_used += file + " "
+            # Read the contents of the source file
+            with open(source_path, "rb") as source_file:
+                # Create the destination file and write the contents
+                with open(destination_path, "wb") as destination_file:
+                    destination_file.write(source_file.read())
+        loader = PyPDFDirectoryLoader(pdf_storage_dir + "/tmp")
+        input_text =  loader.load()
+        embeddings = OpenAIEmbeddings()
+        vector_store = FAISS.from_documents(input_text, embeddings)
+        # Save the files to local disk.
         vector_store.save_local("data_store")
 
-        return {"result": "Database updated"}
+        return {"result": "Database updated\n" + str(files_used)}
 
     except Exception as e:
         return {"result": str(e)}
@@ -126,6 +169,56 @@ async def handle_query(request: Message):
     )
     result_text = chain(request.message)
     return {"result": result_text['answer']}
+
+@app.get(PDF_LIST_ENDPOINT)
+async def get_pdf_list():
+    if not os.path.isdir(pdf_storage_dir):
+        return {"error": "Invalid directory path"}
+
+    files = os.listdir(pdf_storage_dir)
+    content = []
+
+    for file_name in files:
+        content.append(file_name)
+
+    return json.dumps(content)
+
+@app.post(PDF_UPLOAD_ENDPOINT)
+async def upload_pdf(file: UploadFile = UploadFile(...)):
+    # Save the uploaded PDF file
+    print("Uploading of " + file.filename)
+    #text_output = "./storage/txt/output.txt"
+    with open(pdf_storage_dir + "/" + file.filename, "wb") as f:
+        f.write(await file.read())
+
+@app.post(PDF_DELETE_ENDPOINT)
+async def delete_pdf(file_name: str):
+    # Save the uploaded PDF file
+    print("deleting of " + file.filename)
+    try:
+        os.remove(pdf_storage_dir + "/" +file_name)
+        return {"message": f"File '{file_name}' deleted successfully."}
+    except FileNotFoundError:
+        return {"message": f"File '{file_name}' not found."}
+    except Exception as e:
+        return {"message": f"An error occurred while deleting the file: {str(e)}"}
+
+@app.get("/update_file_timestamp")
+async def update_file_timestamp(filename: str):
+    # Specify the directory where the file is located
+
+    # Check if the file exists in the directory
+    file_path = os.path.join(pdf_storage_dir, filename)
+    if os.path.isfile(file_path):
+        # Get the current timestamp
+        current_time = time.time()
+
+        # Update the file's modified timestamp
+        os.utime(file_path, (current_time, current_time))
+
+        return {"message": f"File '{filename}' timestamp updated."}
+    else:
+        return {"message": f"File '{filename}' not found."}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
